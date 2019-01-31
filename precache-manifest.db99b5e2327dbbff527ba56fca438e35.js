3ca2b3875cee58c74c6fdc69566795f866deb6ba
Daniel Kornas <harvi_pl@wp.pl>
2019-01-31 16:18:20 +0100

    Updates

---
+++ precache-manifest.db99b5e2327dbbff527ba56fca438e35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "7680d4d263b2aee27a96",
+    "url": "/weather-app/static/js/main.7680d4d2.chunk.js"
+  },
+  {
+    "revision": "ac9ae1edcdde807d9986",
+    "url": "/weather-app/static/js/1.ac9ae1ed.chunk.js"
+  },
+  {
+    "revision": "7680d4d263b2aee27a96",
+    "url": "/weather-app/static/css/main.b3784170.chunk.css"
+  },
+  {
+    "revision": "bbb30bd42face7752d778417311125cf",
+    "url": "/weather-app/index.html"
+  }
+];
